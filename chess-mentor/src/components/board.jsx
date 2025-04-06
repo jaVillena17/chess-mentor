3e72881c52dd29004348feb225b4490de75df259
jaVillena17 <javillena13@gmail.com>
2025-04-06 17:53:37 +0200

    basic drag and drop

--- chess-mentor/src/components/board.jsx
+++ chess-mentor/src/components/board.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Square } from "./square";
 
 const initialBoard = [
@@ -21,10 +22,64 @@ function calcCoordinatesbyIndex(index, indice){
     return xCoord + "" + y
 }
 
+function translateCoordinates(coord){
+    const x = coordinates.indexOf(coord[0])
+    const y = parseInt(coord[1]) - 1 
+
+    return ({
+        "X": x, 
+        "Y": y
+    })
+}
+
+
+
 export const Board = () =>{
+    const [board, setBoard] = useState(initialBoard)
+    const [draggedPiece, setDraggedPiece] = useState(null)
+    const [validMoves, setValidMoves] = useState([])
+
+    const handleDragStart = (coord, piece) => {
+        setDraggedPiece({
+            coordinates : coord,
+            piece: piece
+        })     
+    }
+
+    const handleOnDrop = (coord) => {
+        let initialDraggedPosition = translateCoordinates(draggedPiece.coordinates)
+        let destinyPosition = translateCoordinates(coord)
+
+        let newBoard = [];
+        //Copiamos vamos copiando los valores en el nuevo board
+        board.forEach((row, indexX) => {
+            let newRow = []
+
+            row.forEach((_, indexY) => {
+
+
+                if(indexX == initialDraggedPosition.X && indexY == initialDraggedPosition.Y){
+                    newRow.push(0)
+                }else if ((indexX == destinyPosition.X && indexY == destinyPosition.Y)){
+                    newRow.push(draggedPiece.piece)
+                }else{
+                    newRow.push(board[indexX][indexY])
+                }
+            })
+
+            newBoard.push(newRow)
+        })
+
+        setBoard(newBoard)
+    }
+    
+
+
+                
+                
     return (
         <div className="flex-board">
-            {initialBoard.map((row, rowIndex) => {
+            {board.map((row, rowIndex) => {
                 return (
                     row.map((piece, colIndex) =>  (
                             <Square 
@@ -32,6 +87,10 @@ export const Board = () =>{
                                 index={calcCoordinatesbyIndex(colIndex, rowIndex)}
                                 piece={piece}
                                 row={rowIndex%2}
+                                onDrop = {handleOnDrop}
+                                onDragStart = {handleDragStart}
+                                onDragOver = {(e) => e.preventDefault()}
+                                validMoves = {validMoves}
                             />)
                     )
             )})}
--- chess-mentor/src/components/square.jsx
+++ chess-mentor/src/components/square.jsx
@@ -1,7 +1,21 @@
-export const Square = ({index, piece, row}) => {
+export const Square = ({index, piece, row, onDrop, onDragStart, onDragOver, validMoves}) => {
+    
+
+
+
+
+    const handleDragStart = () => {
+        onDragStart(index, piece)
+    }
+
+    const handleDrop = (e) => {
+        e.preventDefault()
+        onDrop(index)
+    }
+
     return (
-        <div className="square" id={`${index}`} row={`${row}`}>
-           {piece != 0 && piece} 
+        <div className="square" id={`${index}`} row={`${row}`} onDrop={handleDrop} onDragOver={onDragOver}>
+           <span draggable="true" onDragStart={handleDragStart}>{piece != 0 && piece}</span>
         </div>
     )
 }
