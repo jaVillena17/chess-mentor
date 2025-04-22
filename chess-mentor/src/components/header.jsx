5919fd36415076d625a22454faadb1d7daf99089
jaVillena17 <javillena13@gmail.com>
2025-04-22 18:20:15 +0200

    header updated with links and provisional logo

--- chess-mentor/src/components/header.jsx
+++ chess-mentor/src/components/header.jsx
@@ -1,9 +1,17 @@
 import '../static/css/header.css'
+import { Link } from 'react-router-dom'
+
+const views = ["Inicio", "Jugar", "Social", "FaQ", "Quienés Sómos","Iniciar Sesión"]
 
 export const Header = () => {
     return (
         <nav className="header-nav">
-
+            <img src="/assets/logo.png" alt="Logo" />
+            {views.map((link) => {
+                return (
+                    <Link to={`/${link}`} key={link}>{link}</Link>
+                )
+            })}
         </nav>
     )
 }
