68daa01884b5c586887641f2d5f0f8db70d52b01
jaVillena17 <javillena13@gmail.com>
2025-04-29 20:38:14 +0200

    learning FastApi

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Entidad user
+class User(BaseModel):
+    name: str
+    surname: str
+    url: str
+    age: int
+
+user = {"usuario1" : User(name="Javier", surname="Villena", url="www.nipo.com", age=27)}
 
 @app.get('/')
 async def root():
     return "Hola FastAPI"
 
 @app.get('/curso')
-async def root():
+async def get_curso():
     return {"key" : "value"}
+
+@app.get('/user')
+async def get_user():
+    return user
+
+# uvicorn main:app --reload
+
+# /docs - /redocs
